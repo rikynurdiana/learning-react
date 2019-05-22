ec4025cd74e3e7541c0825fc4f296e40acd97788
riky <nurdiana.riky@gmail.com>
2019-05-22 08:54:32 +0700

    refactor biodata using context

--- src/components/button/BtnCancel.js
+++ src/components/button/BtnCancel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
+import { Link } from 'react-router-dom';
 
 const iconStyle = {
   fontSize: '14px',
@@ -11,16 +12,21 @@ const btnStyle = {
   margin: '0 0 0 0'
 }
 
+const linkStyle = {
+  textDecoration: 'none'
+}
+
 const BtnCancel = (props) => {
-  const { handleCancel, submitted } = props;
+  const { urlCancel, submitted } = props
   return (
-    <Button
-      onClick={handleCancel}
-      variant="contained"
-      style={btnStyle}
-      disabled={submitted}>
-      <Icon style={iconStyle}>arrow_back</Icon> Cancel
-    </Button>
+    <Link to={urlCancel} style={linkStyle}>
+      <Button
+        variant="contained"
+        style={btnStyle}
+        disabled={submitted}>
+        <Icon style={iconStyle}>arrow_back</Icon> Cancel
+      </Button>
+    </Link>
   )
 }
 
--- src/page/crud_biodata/index.js
+++ src/page/crud_biodata/index.js
@@ -5,101 +5,24 @@ import Table from 'components/table'
 import Thead from 'components/table/TableHeader'
 import Tbody from './components/TableData';
 import BtnCreate from 'components/button/BtnCreate';
+import { Provider, Consumer } from './context/Provider'
 
-import api from 'api/Api';
-import methods from 'api/Methods';
-import endpoint from 'api/Endpoint';
-import notification from 'components/Notification';
-
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pageTitle: 'Biodata',
-      isLoading: true,
-      items: []
-    }
-  }
-
-  handleCreate = () => {
-    this.props.history.push('/form-create-crud-biodata')
-  }
-
-  handleEdit = (id) => {
-    this.props.history.push(`/form-edit-crud-biodata/${id}`)
-  }
-
-  handleDelete = async (id) => {
-    let delData = await api.requestApi(`${endpoint.biodata}/${id}`, null, methods.delete)
-    delData.status === 200 ?
-    notification('Success', 'Selected Data Has Been Remove', 'success').then((result) => {this.getListData()}) :
-    notification('Error', 'Failed To Remove Selected Data', 'error')
-  }
-
-  getListData = async () => {
-    let reqData = await api.requestApi(endpoint.biodata, null, methods.get)
-    reqData.status === 200 ?
-    this.setState({ items: reqData.data, isLoading: false }) :
-    notification('Error', 'There is an error on request data', 'error')
-  }
-
-  componentDidMount() {
-    window.scrollTo(0, 0);
-    this.getListData();
-  }
-  
-  render() {
-    return (
-      <WrapperContainer title={this.state.pageTitle}>
-        <BtnCreate handleCreate={this.handleCreate} />
-        {this.state.isLoading === true && <LinearProgress />}
-        <Table>
-          <Thead items={itemHeader} />
-          <Tbody
-            items={this.state.items} 
-            handleDelete={this.handleDelete}
-            handleEdit={this.handleEdit} />
-        </Table>
-      </WrapperContainer>
-    )
-  }
+export default function Index() {
+  window.scrollTo(0, 0);
+  return (
+    <Provider>
+      <Consumer>
+        {(context) => (
+          <WrapperContainer title={context.state.pageTitleIndex}>
+            <BtnCreate urlCreate={context.state.urlCreate} />
+            {context.state.isLoading === true && <LinearProgress />}
+            <Table>
+              <Thead items={context.itemHeader} />
+              <Tbody />
+            </Table>
+          </WrapperContainer>
+        )}
+      </Consumer>
+    </Provider>
+  )
 }
-
-const itemHeader = [
-  {
-    id: 1,
-    text: 'No',
-    align: 'center',
-    style: { width: '50px' }
-  },
-  {
-    id: 2,
-    text: 'Name',
-    align: 'left',
-    style: {}
-  },
-  {
-    id: 3,
-    text: 'Gender',
-    align: 'left',
-    style: {}
-  },
-  {
-    id: 4,
-    text: 'Age',
-    align: 'left',
-    style: {}
-  },
-  {
-    id: 5,
-    text: 'Address',
-    align: 'left',
-    style: {}
-  },
-  {
-    id: 6,
-    text: 'Action',
-    align: 'center',
-    style: {}
-  }
-]
